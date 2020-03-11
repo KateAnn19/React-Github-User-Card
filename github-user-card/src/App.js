c85a97c1d5da5fbcb229872d690e097bb52d0ffa
Kate McGee <mcgeehee@gmail.com>
2020-03-11 09:49:51 -0600

    trying to get bio, location, followers, following

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -5,70 +5,89 @@ import Users from "./users";
 import LambdaLogo from "./lambdalogo.png";
 import GithubLogo from "./githublogo.png";
 
-//what data will I need?
-//I will need user information:
-//name
-//location
-//profile
-//followers
-//following
-//bio
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       users: [],
       followers: [],
+      moreInfo: [],
       followersInfo: '',
-      followingInfo: ''
+      followingInfo: '',
+      // bios:'',
+      name: '',
+      location: '',
+      Followers: '',
+      following: ''
     };
   }
 
   componentDidMount() {
+
+
     axios
       .get("https://api.github.com/users/KateAnn19")
       .then(res => {
-        console.log(res.data);
-
         this.setState({
           users: res.data
         });
       })
       .catch(err => console.log(err.message));
 
+
+
+
+
       axios
       .get("https://api.github.com/users/KateAnn19/followers")
       .then(res => {
-        console.log(res);
-        this.setState({
-          followers: res.data
-        })
-      })
-      .catch(err => console.log(err.message));
+        console.log('this is response from followers', res)
+        res.data.map(followers => (
+           
 
-      axios
-      .get("https://api.github.com/users/KateAnn19/followers/followers_url")
-      .then(res => {
-        console.log(res);
-        this.setState({
-          followersInfo: res.data
-        })
-      })
-      .catch(err => console.log(err.message));
 
-      axios
-      .get("https://api.github.com/users/KateAnn19/followers/following_url")
-      .then(res => {
-        console.log(res);
+
+
+           axios
+            .get(followers.url)
+            .then(response => {
+             console.log("this is response", response)
+            //  response.map(foll =>(
+
+               this.setState({
+              //  moreInfo: response.data,
+              //  followers: response.data.followers,
+               //following: response.data.following,
+               //location: response.data.location,
+               bios: response.data.bio,
+               name: response.data.name,
+               location: response.data.location,
+               Followers: response.data.followers,
+               following: response.data.following
+             })
+            })
+            .catch(err => console.log(err.message))
+
+        ))
+             
+            
+           
+        //))
+        
         this.setState({
-          followeringInfo: res.data
+          followers: res.data,
+          moreInfo: res.data.followers
         })
       })
       .catch(err => console.log(err.message));
   }
 
-  
+  // you can chain your axio calls to map through your friends and map again to set their url in a state array for you to use. Then you can map that state array exactly how you did your own card
+
+//map.axios friends
+//map.axios friends again
+//set url in state array
+//map through state array 
 
   render() {
     return (
@@ -80,7 +99,15 @@ class App extends React.Component {
           </p>
           <img src={GithubLogo} alt="GitHub Logo" />
         </div>
-        <Users usersInfo={this.state.users} followersInfo={this.state.followers}/>
+        <Users 
+        usersInfo={this.state.users} 
+        location={this.state.location}
+        followingNumbers={this.state.following}
+        followersNumbers={this.state.followers}
+        followersInfo={this.state.followers}
+        url={this.state.url} 
+        bios={this.state.bios}
+        Location={this.state.location}/>
       </div>
     );
   }
--- github-user-card/src/users.js
+++ github-user-card/src/users.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Users = props => {
-  console.log("this is props", props);
+    console.log(props)
+  
   return (
     <div className="cards">
       <div className="card">
@@ -21,17 +22,36 @@ const Users = props => {
       </div>
 
       {props.followersInfo.map(follower => (
+
+        
+
+
+
         <div key={follower.id} className="card">
           <div className="card-info">
             <img src={follower.avatar_url} alt={follower.login} />
+            <h3 className="name">Name</h3>
             <h2 className="username">{follower.login}</h2>
-            <p>Location: {follower.location}</p>
-            <p>Followers: {follower.followers_url}</p>
-            <p>Following: {follower.following_url}</p>
-            {/* <p>Bio: {props.follower.bio}</p> */}
+            <p>Bio:{follower.bios}</p>
+            <p>Location: {props.location}</p>
+            <p>Followers: </p>
+            <p>Following: </p>
           </div>
         </div>
       ))}
+
+{/* {props.bios.map(bio => (
+        <div>
+          <div>
+          <p>Bio: {bio.bio}</p>
+          </div>
+        </div>
+      ))} */}
+
+      
+          <div>
+            <p>Bio: {props.bio}</p>
+          </div>
     </div>
   );
 };
